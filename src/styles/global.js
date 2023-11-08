69c7a255b2ae81c10e4db93031cbd9dbb24f4d66
André Silveira <andre.silveira1silva@gmail.com>
2023-11-08 15:39:43 -0300

    styles: Global styles and themes.

---
+++ src/styles/global.js
@@ -0,0 +1,34 @@
+import { createGlobalStyle } from 'styled-components'
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  :root {
+    font-size: 62.5%;
+  }
+
+  body {
+    font-family: 'Roboto', sans-serif;
+    font-size: 1.6rem;
+
+    background-color: ${({ theme }) => theme.COLORS.Dark400};
+    color: ${({ theme }) => theme.COLORS.Light100};
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  button, a {
+    cursor: pointer;
+    transition: 0.2;
+  }
+
+  button:hover, a:hover {
+    filter: brigntness(0.9);
+  }
+`
