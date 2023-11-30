5bc91f3862f9e3e82923aea4c76b95fad233b19a
André Silveira <andre.silveira1silva@gmail.com>
2023-11-29 22:36:19 -0300

    feat: LocalStorage Save/Delete/Load user auth

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from 'react'
+import { createContext, useContext, useState, useEffect } from 'react'
 
 import { api } from '../services/api'
 
@@ -12,6 +12,9 @@ function AuthProvider({ children }) {
       const response = await api.post('/sessions', { email, password })
       const { user, token } = response.data
 
+      localStorage.setItem('@foodexplorer:user', JSON.stringify(user))
+      localStorage.setItem('@foodexplorer:token', token)
+
       api.defaults.headers.authorization = `Bearer ${token}`
       setData({ user, token })
     } catch (error) {
@@ -23,8 +26,29 @@ function AuthProvider({ children }) {
     }
   }
 
+  function signOut() {
+    localStorage.removeItem('@foodexplorer:token')
+    localStorage.removeItem('@foodexplorer:user')
+
+    setData({})
+  }
+
+  useEffect(() => {
+    const token = localStorage.getItem('@foodexplorer:token')
+    const user = localStorage.getItem('@foodexplorer:user')
+
+    if (token && user) {
+      api.defaults.headers.authorization = `Bearer ${token}`
+
+      setData({
+        token,
+        user: JSON.parse(user),
+      })
+    }
+  }, [])
+
   return (
-    <AuthContext.Provider value={{ signIn, user: data.user }}>
+    <AuthContext.Provider value={{ signIn, signOut, user: data.user }}>
       {children}
     </AuthContext.Provider>
   )
