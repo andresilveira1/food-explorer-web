145673dea43712f14a3e8b0194399b0be420b426
André Silveira <andre.silveira1silva@gmail.com>
2023-11-30 23:19:49 -0300

    style: Added button hover

--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -13,7 +13,15 @@ export const Container = styled.button`
   background-color: ${({ theme }) => theme.COLORS.Tomato400};
   color: ${({ theme }) => theme.COLORS.Light100};
 
+  transition: all 500ms cubic-bezier(0.77, 0, 0.175, 1);
+
   &:disabled {
     opacity: 0.5;
+  }
+
+  &:hover {
+    background-color: ${({ theme }) => theme.COLORS.Tomato300};
+
+    transition: 0.5s;
   }
 `
