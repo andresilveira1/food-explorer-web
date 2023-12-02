1b4a983837e462ce04574670853a8285d0a99307
André Silveira <andre.silveira1silva@gmail.com>
2023-12-01 22:17:55 -0300

    feat: Update Routes

--- src/routes/app.routes.jsx
+++ src/routes/app.routes.jsx
@@ -2,12 +2,16 @@ import { Routes, Route } from 'react-router-dom'
 
 import { Home } from '../pages/Home'
 import { Details } from '../pages/Details'
+import { NewDish } from '../pages/NewDish'
+import { UpdateDish } from '../pages/UpdateDish'
 
 export function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/details/:id" element={<Details />} />
+      <Route path="/newdish" element={<NewDish />} />
+      <Route path="/updatedish" element={<UpdateDish />} />
     </Routes>
   )
 }
