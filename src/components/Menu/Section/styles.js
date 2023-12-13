0dede26b223f6dde1e55cf070e4ec51032e28dd5
André Silveira <andre.silveira1silva@gmail.com>
2023-12-13 10:31:37 -0300

    style: update styles

--- src/components/Menu/Section/styles.js
+++ src/components/Menu/Section/styles.js
@@ -23,7 +23,49 @@ export const Container = styled.section`
 
   .swiper-button-next,
   .swiper-button-prev {
-    color: ${({ theme }) => theme.COLORS.Light100};
+    display: none;
+  }
+
+  @media (min-width: ${DEVICE_BREAKPOINTS.LG}) {
+    .swiper-button-next,
+    .swiper-button-prev {
+      display: flex;
+
+      height: 100%;
+      width: 15rem;
+
+      color: ${({ theme }) => theme.COLORS.Light100};
+    }
+
+    .swiper-button-prev {
+      left: -5px;
+      top: 1.375rem;
+
+      padding-left: 1.9rem;
+
+      justify-content: flex-start;
+
+      background: linear-gradient(
+        90deg,
+        #000a0f 0%,
+        rgba(0, 10, 15, 0.27) 100%
+      );
+    }
+
+    .swiper-button-next {
+      right: 0;
+      top: 1.375rem;
+
+      padding-right: 1.9rem;
+
+      justify-content: flex-end;
+
+      background: linear-gradient(
+        90deg,
+        rgba(0, 10, 15, 0.27) 0%,
+        #000a0f 100%
+      );
+    }
   }
 
   @media (min-width: ${DEVICE_BREAKPOINTS.XL}) {
