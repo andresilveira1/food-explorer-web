08d63c8957ca78b918d97b2a8526287204e64597
André Silveira <andre.silveira1silva@gmail.com>
2023-12-21 21:49:51 -0300

    feat: Added interaction with backend

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,11 +1,39 @@
 import { Hexagon } from '@phosphor-icons/react'
 import { Container, Form } from './styles'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import { useState } from 'react'
+import { api } from '../../services/api'
 
 import { Input } from '../../components/Input'
 import { Button } from '../../components/Button'
 
 export function SignUp() {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const navigate = useNavigate()
+
+  function handleSignUp() {
+    if (!name || !email || !password) {
+      return alert('Preencha todos os campos!')
+    }
+
+    api
+      .post('/users', { name, email, password })
+      .then(() => {
+        alert('Usuário cadastrado com sucesso!')
+        navigate('/')
+      })
+      .catch((error) => {
+        if (error.response) {
+          alert(error.response.data.message)
+        } else {
+          alert('Não foi possível cadastrar.')
+        }
+      })
+  }
+
   return (
     <Container>
       <h1>
@@ -17,19 +45,29 @@ export function SignUp() {
         <h2>Crie sua conta</h2>
 
         <label htmlFor="name">Seu nome</label>
-        <Input id="name" placeholder="Exemplo: Maria da Silva" />
+        <Input
+          id="name"
+          placeholder="Exemplo: Maria da Silva"
+          onChange={(e) => setName(e.target.value)}
+        />
 
         <label htmlFor="email">Email</label>
-        <Input id="email" type="email" placeholder="exemplo@exemplo.com.br" />
+        <Input
+          id="email"
+          type="email"
+          placeholder="exemplo@exemplo.com.br"
+          onChange={(e) => setEmail(e.target.value)}
+        />
 
         <label htmlFor="password">Senha</label>
         <Input
           id="password"
           type="password"
           placeholder="No mínimo 6 caracteres"
+          onChange={(e) => setPassword(e.target.value)}
         />
 
-        <Button type="submit" title="Entrar" />
+        <Button title="Entrar" onClick={handleSignUp} />
 
         <Link to="/">Já tenho uma conta</Link>
       </Form>
