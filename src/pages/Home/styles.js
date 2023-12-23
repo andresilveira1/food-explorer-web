d6c437a02cefcc7945ed41c628733cfd3f363ae4
André Silveira <andre.silveira1silva@gmail.com>
2023-12-23 20:31:16 -0300

    style: update styles

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -18,150 +18,6 @@ export const Container = styled.div`
   }
 `
 
-export const Main = styled.main`
-  grid-area: content;
-
-  overflow-y: scroll;
-
-  > section {
-    margin: 0 0 2.4rem 2.4rem;
-
-    div {
-      display: flex;
-      gap: 1.6rem;
-    }
-
-    .swiper-wrapper {
-      gap: 1.6rem;
-    }
-
-    .swiper-button-next,
-    .swiper-button-prev {
-      color: ${({ theme }) => theme.COLORS.Light100};
-    }
-
-    .slide-item {
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      gap: 1.2rem;
-
-      position: relative;
-
-      padding: 2.4rem;
-
-      height: 29.2rem;
-      width: 21rem;
-
-      margin: 0;
-
-      border-radius: 0.8rem;
-
-      background: ${({ theme }) => theme.COLORS.Dark900};
-
-      .fav {
-        position: absolute;
-        top: 1.6rem;
-        right: 1.6rem;
-
-        font-size: 3rem;
-
-        svg {
-          color: ${({ theme }) => theme.COLORS.Light300};
-        }
-      }
-
-      .slide-img {
-        width: 8.8rem;
-        height: 8.8rem;
-
-        object-fit: cover;
-      }
-
-      a {
-        font-family: 'Poppins', sans-serif;
-        font-weight: 500;
-        font-size: 1.4rem;
-        color: ${({ theme }) => theme.COLORS.Light300};
-
-        display: flex;
-        align-items: center;
-      }
-
-      h2 {
-        display: none;
-      }
-
-      p {
-        color: ${({ theme }) => theme.COLORS.Cake100};
-      }
-
-      .add-product {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-
-        width: 100%;
-        height: 8rem;
-      }
-    }
-  }
-
-  @media (min-width: ${DEVICE_BREAKPOINTS.XL}) {
-    > section {
-      max-width: 112rem;
-
-      margin: 0 auto 2.4rem;
-
-      .swiper-wrapper {
-        gap: 2.7rem;
-      }
-
-      .slide-item {
-        width: 30.4rem;
-        height: 46.2rem;
-
-        gap: 1.5rem;
-
-        .slide-img {
-          width: 17.6rem;
-          height: 17.6rem;
-        }
-
-        a {
-          font-size: 2.4rem;
-          font-weight: 700;
-          white-space: nowrap;
-        }
-
-        h2 {
-          display: block;
-
-          font-size: 1.4rem;
-          font-weight: 400;
-          line-height: 160%;
-          color: ${({ theme }) => theme.COLORS.Light400};
-          text-align: center;
-        }
-
-        p {
-          font-size: 3.2rem;
-        }
-
-        .add-product {
-          flex-direction: row;
-
-          width: 20.8rem;
-
-          button {
-            height: 4.8rem;
-          }
-        }
-      }
-    }
-  }
-`
-
 export const Banner = styled.div`
   grid-area: content;
 
@@ -317,4 +173,10 @@ export const Banner = styled.div`
       }
     }
   }
+`
+
+export const Main = styled.main`
+  grid-area: content;
+
+  overflow-y: auto;
 `
