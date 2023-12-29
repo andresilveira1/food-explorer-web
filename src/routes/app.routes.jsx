2799f94f83cf576c6325fec56aa58ba0e5eb16f2
André Silveira <andre.silveira1silva@gmail.com>
2023-12-28 22:44:11 -0300

    routes: Added payment route

--- src/routes/app.routes.jsx
+++ src/routes/app.routes.jsx
@@ -7,6 +7,7 @@ import { Details } from '../pages/Details'
 import { NewDish } from '../pages/NewDish'
 import { UpdateDish } from '../pages/UpdateDish'
 import { Favorites } from '../pages/Favorites'
+import { Payment } from '../pages/Payment'
 
 export function AppRoutes() {
   const { user } = useAuth()
@@ -15,7 +16,8 @@ export function AppRoutes() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/details/:id" element={<Details />} />
-      <Route path="favorites" element={<Favorites />} />
+      <Route path="/favorites" element={<Favorites />} />
+      <Route path="/payment" element={<Payment />} />
       {user.admin && <Route path="/newdish" element={<NewDish />} />}
       {user.admin && <Route path="/updatedish/:id" element={<UpdateDish />} />}
     </Routes>
