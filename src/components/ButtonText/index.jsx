fee210687bf030676c0f67944eb57d80478f8a03
André Silveira <andre.silveira1silva@gmail.com>
2023-12-28 22:46:07 -0300

    feat: Added a children as dependency

--- src/components/ButtonText/index.jsx
+++ src/components/ButtonText/index.jsx
@@ -1,9 +1,10 @@
 import { Container } from './styles'
 
-export function ButtonText({ title, icon: Icon, ...rest }) {
+export function ButtonText({ title, icon: Icon, children, ...rest }) {
   return (
     <Container type="button" {...rest}>
       {Icon && <Icon />}
+      {children}
       {title}
     </Container>
   )
