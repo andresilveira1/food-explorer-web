1eb06d6195762a100e245f11737e3c9241dbc6a9
André Silveira <andre.silveira1silva@gmail.com>
2024-01-06 23:08:15 -0300

    feat: Update Input component

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -1,10 +1,11 @@
 import { Container } from './styles'
 
-export function Input({ icon: Icon, ...rest }) {
+export function Input({ icon: Icon, children, ...rest }) {
   return (
     <Container>
       {Icon && <Icon size={24} />}
       <input {...rest} />
+      {children}
     </Container>
   )
 }
