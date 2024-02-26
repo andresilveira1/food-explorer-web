f382a02d52e3153aeb52f1069759d131940ad529
André Silveira <andre.silveira1silva@gmail.com>
2024-02-25 22:41:01 -0300

    feat: add validation route

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,10 +2,30 @@ import { BrowserRouter } from 'react-router-dom'
 
 import { useAuth } from '../hooks/auth'
 import { AuthRoutes } from './auth.routes'
-import { AppRoutes } from './app.routes'
+import { AdminRoutes } from './admin.routes'
+import { CustomerRoutes } from './customer.routes'
+import { useEffect } from 'react'
+import { api } from '../services/api'
 
 export function Routes() {
-  const { user } = useAuth()
+  const { user, signOut } = useAuth()
 
-  return <BrowserRouter>{user ? <AppRoutes /> : <AuthRoutes />}</BrowserRouter>
+  useEffect(() => {
+    api.get('/users/validated').catch((error) => {
+      if (error.response?.status === 401) {
+        signOut()
+      }
+    })
+  }, [])
+
+  function AccessRoute() {
+    switch (user.admin) {
+      case 1:
+        return <AdminRoutes />
+      case 0:
+        return <CustomerRoutes />
+    }
+  }
+
+  return <BrowserRouter>{user ? AccessRoute() : <AuthRoutes />}</BrowserRouter>
 }
